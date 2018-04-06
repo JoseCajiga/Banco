bfc9457091e04642fcc34cec5c6f50d657193026
JoseCajiga <josealfredo_cajiga@live.com.mx>
2018-04-06 13:12:10 -0700

    Release1
    model

---
+++ MiBanco/src/main/java/mx/MiBanco/MiBancoApplication.java
@@ -0,0 +1,12 @@
+package mx.MiBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiBancoApplication.class, args);
+	}
+}
